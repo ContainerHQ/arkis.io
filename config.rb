417ee5f865f3555663141d28710b85bc4ef04317
Adrien Folie <folie.adrien@gmail.com>
2015-10-13 14:21:18 +0200

    add relative assets

--- config.rb
+++ config.rb
@@ -53,6 +53,10 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+# Github pages require relative links
+activate :relative_assets
+set :relative_links, true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -63,9 +67,6 @@ configure :build do
 
   # Enable cache buster
   # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
